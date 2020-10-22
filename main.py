a834edfd579171b13dd1251bf1abee3993a8faa4
Evelin Rodriguez Suarez <evelin992009@gmail.com>
2020-10-22 19:37:06 +0000

    funciones for

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''
 def funcionfor01(a):
   for x in range(a):
     print (x)
@@ -6,4 +7,21 @@ def funcionfor02(a,b):
     print (x)    
 #funcion para imprimir hasta 10
 #funcionfor01(10)
-funcionfor02(10,5)
+#funcion imprimir hasta 15
+#funcionfor02(5, 15)
+
+#conteo de personas
+personas = ["luisa" , "ana" , "maria" ]
+for x in personas:
+  print(x)
+'''
+#es un for(i=0 ; i<=a ; i++)(sum=i)
+def funcionWhile(a):
+  suma=0
+  i=0
+  while i <= a:
+    suma= suma + i #equivalente a suma+=i
+    i+= 1
+    print(suma)
+
+funcionWhile(10)
