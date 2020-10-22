9966c55d0e2e9ac0e228f4e6c837bbfd7a9657a2
Evelin Rodriguez Suarez <evelin992009@gmail.com>
2020-10-22 19:24:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def funcionfor01(a):
+  for x in range(a):
+    print (x)
+def funcionfor02(a,b):
+  for x in range(a,b):
+    print (x)    
+#funcion para imprimir hasta 10
+#funcionfor01(10)
+funcionfor02(10,5)
